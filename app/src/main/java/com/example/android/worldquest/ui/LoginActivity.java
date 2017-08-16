ef778bb0c750803b861205ddf490ee3c251f405d
Ryan Aquilina <ryanaquilina001@gmail.com>
2017-08-16 18:52:52 +0200

    Add registration and login activity

--- app/src/main/java/com/example/android/worldquest/ui/LoginActivity.java
+++ app/src/main/java/com/example/android/worldquest/ui/LoginActivity.java
@@ -1,11 +1,28 @@
 package com.example.android.worldquest.ui;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.android.worldquest.R;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -13,15 +30,85 @@ import android.os.Bundle;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
+        // Get Firebase instance
+        mAuth = FirebaseAuth.getInstance();
+
+        // Get views references
+        final AutoCompleteTextView emailAddressTextView = (AutoCompleteTextView) findViewById(R.id.email);
+        final EditText passwordEditTextView = (EditText) findViewById(R.id.password);
+        Button signInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button registerButton = (Button) findViewById(R.id.register_button);
+
+        // Set views functionality
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailAddress = emailAddressTextView.getText().toString();
+                String password = passwordEditTextView.getText().toString();
+
+                if(emailAddress.isEmpty()|| password.isEmpty()){
+                    showSnackBar("Email or Password is incorrect");
+                }
+                else{
+                    signInWithEmailAndPassword(emailAddress, password);
+                }
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        //Check if user is already logged in
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * Show a message
+     * @param message
+     */
+    private void showSnackBar(String message){
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator_layout), message, Snackbar.LENGTH_SHORT);
+        snackbar.show();
+    }
+
+    private void signInWithEmailAndPassword(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            // Sign in successful
+                            showSnackBar("Sign in successful");
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            // Sign in unsuccessful
+                            showSnackBar("Sign in unsuccessful");
+                        }
+                    }
+                });
     }
 }
 
---
+++ app/src/main/java/com/example/android/worldquest/ui/RegisterActivity.java
@@ -0,0 +1,81 @@
+package com.example.android.worldquest.ui;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.worldquest.R;
+
+/**
+ * Created by SterlingRyan on 8/16/2017.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_email_layout);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Get view references
+        final EditText emailAddressEditText = (EditText) findViewById(R.id.email);
+        EditText nameAndSurnameEditText = (EditText) findViewById(R.id.name);
+        final EditText passwordEditText = (EditText) findViewById(R.id.password);
+        Button registerButton = (Button) findViewById(R.id.button_create);
+
+        //set view functionality
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailAddress = emailAddressEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if(emailAddress.isEmpty()|| password.isEmpty()){
+                    showToast("Email or Password is incorrect");
+                }
+                else{
+                    registerWithEmailAndPassword(emailAddress, password);
+                }
+            }
+        });
+    }
+
+    private void registerWithEmailAndPassword(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            // Sign in success therefore sign in
+                            showToast("Sign in successful");
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            showToast("Sign in unsuccessful");
+                        }
+                    }
+                });
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
